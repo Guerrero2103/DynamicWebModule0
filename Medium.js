a17b873a699165b4c2e4312b14fb82da609abc7d
Guerrero2103 <abdullahguerrero111@gmail.com>
2025-02-11 11:29:03 +0100

    Opdrachtmedium

---
+++ Medium.js
@@ -0,0 +1,16 @@
+
+
+
+
+'use strict';
+        function optellen() {
+
+            var getal1 = parseInt(document.getElementById('getal1').value);
+            var getal2 = parseInt(document.getElementById('getal2').value);
+
+
+
+            alert((getal1 + getal2));
+        }
+
+
